55a783448efd1b0770d3fc95dbdf36927826bb93
HarshKarnaa818981 <harshaneeraj0@gmail.com>
2025-07-31 01:11:28 +0530

    Initial commit: uploaded my first Python files

---
+++ Day 1.py
@@ -0,0 +1,30 @@
+print('Harsha')
+print("Hello world")
+name = 'Harsha'
+age = 20
+is_student = True 
+height = 6.0
+print ("My name is", name, "and I am", age, "years old")
+print("Is student:",is_student)
+print("My height is",height)
+x = 10
+print(type(x))
+y = 3.14
+print(type(y))
+
+import keyword 
+print(keyword.kwlist)
+
+def greet():
+    print("Hello!")
+greet()
+
+def greet(name):
+    print("Hello", name)
+
+greet("Harsha")  # Output: Hello Harsha
+
+def name():
+    print('Harsha')
+name()
+    
